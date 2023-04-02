023db4dff482bfe98fc679754e66922d5cf1f171
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-02 13:50:09 +0100

    update

---
+++ Pages/Identity/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehicle_Parking_Management_System.Pages.Identity
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
