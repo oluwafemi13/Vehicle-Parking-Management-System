36facc21b5ad8bc4457768e85594dbc5f50fe346
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-03 01:00:27 +0100

    update

--- Pages/Identity/Register.cshtml.cs
+++ Pages/Identity/Register.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Vehicle_Parking_Management_System.Pages.DTO;
 
 namespace Vehicle_Parking_Management_System.Pages.Identity
 {
+    [BindProperties]
     public class RegisterModel : PageModel
     {
+        public Register register { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
