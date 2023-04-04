ed9dba312b047d1ee9b448f2af3b0ceb90ac14f0
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-04 18:52:06 +0100

    update

--- Pages/Identity/Register.cshtml.cs
+++ Pages/Identity/Register.cshtml.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
 using Vehicle_Parking_Management_System.Pages.DTO;
 
 namespace Vehicle_Parking_Management_System.Pages.Identity
@@ -7,6 +9,19 @@ namespace Vehicle_Parking_Management_System.Pages.Identity
     [BindProperties]
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RegisterModel> _logger;
+
+        public RegisterModel(UserManager<IdentityUser> userManager, 
+                             RoleManager<IdentityRole> roleManager,
+                             ILogger<RegisterModel> logger)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
         public Register register { get; set; }
         public void OnGet()
         {
