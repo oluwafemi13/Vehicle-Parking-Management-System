303d2f383ce1a294cc6bf62c0fd2a840c3e7baa3
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-14 21:21:26 +0100

    update

---
+++ Pages/Admin/UserManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehicle_Parking_Management_System.Pages.Admin
+{
+    public class UserManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
