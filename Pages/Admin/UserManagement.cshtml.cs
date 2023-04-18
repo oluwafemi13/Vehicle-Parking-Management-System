603d6734112e524b51824bf1b6711a056a6e608b
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-18 16:13:50 +0100

    update

--- Pages/Admin/UserManagement.cshtml.cs
+++ Pages/Admin/UserManagement.cshtml.cs
@@ -1,12 +1,48 @@
+using DataAccess.Persistence.Interface;
+using DataAccess.Persistence.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System.Security.Claims;
 
 namespace Vehicle_Parking_Management_System.Pages.Admin
 {
+    [BindProperties]
     public class UserManagementModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserManagementModel(IUnitOfWork unitOfWork, 
+                                   UserManager<ApplicationUser> userManager, 
+                                   SignInManager<ApplicationUser> signInManager)
         {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IEnumerable<ApplicationUser> users { get; set; }
+        
+
+
+        public void OnGet(string id)
+        {
+            /*var ClaimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = ClaimIdentity.FindFirst(ClaimTypes.NameIdentifier);*/
+
+            users = _userManager.Users.ToList();
+
+
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            return RedirectToPage();
         }
     }
 }
