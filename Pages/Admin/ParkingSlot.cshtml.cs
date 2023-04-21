97174962524cdfcd453e04afe65b2826d30a9adb
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-21 21:35:44 +0100

    update

--- Pages/Admin/ParkingSlot.cshtml.cs
+++ Pages/Admin/ParkingSlot.cshtml.cs
@@ -2,6 +2,7 @@ using DataAccess.Persistence.Interface;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
 using Models;
 
 namespace Vehicle_Parking_Management_System.Pages.Admin
@@ -22,7 +23,23 @@ namespace Vehicle_Parking_Management_System.Pages.Admin
             _SignInManager = signInManager;
         }
 
-        public ParkingSlot slot { get; set; }
+        public class InputModel
+        {
+            public int Id { get; set; }
+            [Required]
+            public string SlotName { get; set; }
+            [Required]
+            public string SlotType { get; set; }
+            [Required]
+            public int SlotNumber { get; set; }
+
+            [Required]
+            public string Status { get; set; }
+        }
+        
+
+
+        public InputModel slot { get; set; }
 
         public IEnumerable<ParkingSlot> slots { get; set; }
         public void OnGet()
@@ -43,9 +60,18 @@ namespace Vehicle_Parking_Management_System.Pages.Admin
         {
             if(ModelState.IsValid)
             {
-
+                var Slot = new ParkingSlot();
+                Slot.Status = slot.Status;
+                Slot.SlotNumber = slot.SlotNumber;
+                Slot.SlotName = slot.SlotName;
+                Slot.Id = slot.Id;
+                Slot.SlotType = slot.SlotType;
+                _unitOfWork.Parking.AddAsync(Slot);
+                _unitOfWork.Commit();
+                
+                
             }
-            return RedirectToPage("Dashboard");
+            return RedirectToPage("ParkingSlot");
         }
     }
 }
