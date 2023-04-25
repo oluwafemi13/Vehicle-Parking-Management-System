cabd6fca48776b6fc780ba552f044ce6eb3e4ac1
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-25 15:45:37 +0100

    finishing touches Customer

---
+++ Pages/Customer/BookingRequest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehicle_Parking_Management_System.Pages.Customer
+{
+    public class BookingRequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
