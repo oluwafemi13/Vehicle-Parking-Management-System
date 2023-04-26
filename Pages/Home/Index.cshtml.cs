c164b2382eb8c9263a8478285c573e9b80e07a20
IKECHUKWU <sundayjabikem@gmail.com>
2023-04-26 10:57:44 +0100

    update

---
+++ Pages/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vehicle_Parking_Management_System.Pages.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
